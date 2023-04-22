b7504c0fa88537f2d042e8da19cf5d1eb8a9fcd8
Doug Ross <dross.canada@gmail.com>
2023-04-21 20:53:36 -0400

    added client.js file to hold connect function

---
+++ client.js
@@ -0,0 +1,18 @@
+const net = require("net");
+const connect = function () {
+  const conn = net.createConnection({
+    host: "192.168.2.84",
+    port: 50541, 
+  });
+
+
+  conn.on("data", (data) => {
+    console.log("Server says:", data);
+  });
+
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+module.exports = connect; 
